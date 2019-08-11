5f72af1b068c7c7d3d56b31e8687ee9eac7bcb3b
Mitch Weaver <20451170+MitchWeaver@users.noreply.github.com>
2019-08-11 12:36:33 -0500

    update mkiso.sh

--- mkiso.sh
+++ mkiso.sh
@@ -4,35 +4,36 @@
 #
 # create a ramfs iso of our rootfs
 #
-# Requires: bonsai-{core,kernel,init,bpm}, syslinux
+# Requires: bonsai-{core,kernel,init}, syslinux
 # -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
-# functions
+
 msg() { printf "%s\n" "→ $*" ; }
-# -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
-# config
+
+# -*-*- config -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
 root=~/.local/bonsai
 builddir="$PWD"/build
 bonsai=$root/src/bonsai
 rm -r "${builddir:?}" 2>/dev/null ||: ; mkdir -p "$builddir"
 # -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
 msg 'relinking world...'
 $bonsai --relink-world --chroot
-# -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
 msg 'zipping up the root file system...'
 cd "$root"
 find . | cpio -R root:root -H newc -o | gzip > "$builddir"/rootfs.gz
-# -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
 msg 'copying kernel image...'
 cd "$builddir"
 cp -f "$root"/src/pkgs/bonsai-kernel/vmlinuz ./vmlinuz
-# -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
 msg 'copying bootloader...'
 cp -f "$root"/src/pkgs/syslinux/isolinux.bin ./isolinux.bin
 cp -f "$root"/src/pkgs/syslinux/ldlinux.c32  ./ldlinux.c32
-# -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
 msg 'creating syslinux config...'
 echo 'default vmlinuz initrd=rootfs.gz' > isolinux.cfg
-# -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
 msg 'creating iso...'
 xorriso \
     -as mkisofs \
@@ -42,4 +43,3 @@ xorriso \
     -no-emul-boot \
     -boot-load-size 4 \
     -boot-info-table .
-# -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
